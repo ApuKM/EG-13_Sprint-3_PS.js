04f1c1639a7038913d2cf3099e8b238242433467
ApuKM <apukumar180@gmail.com>
2026-09-08 20:08:33 +0600

    Solved 10 given problems: learned two pointers, Dutch National Flag algorithm

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -88,3 +88,168 @@ var canConstruct = function (ransomNote, magazine) {
   return true;
 };
 // console.log(canConstruct("aa", "aab"));
+
+// 05. Majority Element
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  // your code goes here
+  const count = {};
+
+  for (let num of nums) {
+    count[num] = (count[num] || 0) + 1;
+
+    if (count[num] > nums.length / 2) {
+      return num;
+    }
+  }
+};
+// console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
+
+// 06. 3Sum
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  // your code goes here
+  const result = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        while (nums[left] === nums[left - 1]) {
+          left++;
+        }
+        while (nums[right] === nums[right + 1]) {
+          right--;
+        }
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return result;
+};
+// console.log(majorityElement([-1, 0, 1, 2, -1, -4]));
+
+// 07. Subarray Sum Equals K
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  // your code goes here
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+
+      if (sum === k) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+// console.log(subarraySum([1, 1, 1], 2));
+
+// 08. Top K Frequent Elements
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  // your code goes here
+  const count = {};
+
+  for (let num of nums) {
+    count[num] = (count[num] || 0) + 1;
+  }
+
+  return Object.entries(count)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, k)
+    .map(([num]) => Number(num));
+};
+// console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+
+// 09. Longest Consecutive Sequence
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+  const set = new Set(nums);
+  let longest = 0;
+
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentLength = 1;
+
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentLength++;
+      }
+
+      longest = Math.max(longest, currentLength);
+    }
+  }
+
+  return longest;
+};
+// console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
+
+// 10. Sort Colors
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let left = 0;
+    let current = 0;
+    let right = nums.length - 1;
+
+    while (current <= right) {
+        if (nums[current] === 0) {
+            [nums[left], nums[current]] = [nums[current], nums[left]];
+
+            left++;
+            current++;
+        } 
+        else if (nums[current] === 1) {
+            current++;
+        } 
+        else {
+            [nums[current], nums[right]] = [nums[right], nums[current]];
+
+            right--;
+        }
+    }
+};
+const nums = [2, 0, 2, 1, 1, 0];
+sortColors(nums); 
+console.log(nums);
+
